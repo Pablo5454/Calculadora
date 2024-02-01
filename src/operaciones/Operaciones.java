585bde7333055e291fc98da36da382395f51cb3c
1AW3-16 <1AW3-16@HZ110AAB-1AW3.zornotzalh.eus>
2024-02-01 12:08:36 +0100

    commit1

---
+++ src/operaciones/Operaciones.java
@@ -0,0 +1,74 @@
+package operaciones;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Operaciones{
+    	
+	private Object LOGGER;
+
+	/**
+	 * Realiza la operación "sumar".
+	 * @param Recibe un número entero desde la variable "valor1"
+	 * @param Recibe un número entero desde la variable "valor2"
+	 * @return Devuelve la suma de ambos
+	 */
+	
+	public int sumar (int valor1, int valor2){
+        int numero;
+        numero = valor1 + valor2;
+        return numero;
+    }
+    
+	/**
+	 * Realiza la operación "restar".
+	 * @param Recibe un número entero desde la variable "valor1"
+	 * @param Recibe un número entero desde la variable "valor2"
+	 * @return Devuelve la resta de ambos
+	 */
+	
+    public int restar (int valor1, int valor2){
+        int numero;
+        numero = valor1 - valor2;
+        return numero;
+    }
+
+    /**
+	 * Realiza la operación "multiplicar".
+	 * @param Recibe un número entero desde la variable "valor1"
+	 * @param Recibe un número entero desde la variable "valor2"
+	 * @return Devuelve la multiplicación de ambos
+	 */
+    
+    public int multiplicar (int valor1, int valor2){
+        int numero;
+        numero = valor1 * valor2;
+        return numero;
+    }
+    
+    /**
+	 * Realiza la operación "dividir".
+	 * @param Recibe un número entero desde la variable "valor1"
+	 * @param Recibe un número entero desde la variable "valor2"
+	 * @return Devuelve la división de ambos
+	 */
+    
+    public int dividir (int valor1, int valor2){
+        int numero;
+        numero = valor1 / valor2;
+        return numero;
+    
+    }
+    /**
+	 * Realiza la operación "resto".
+	 * @param Recibe un número entero desde la variable "valor1"
+	 * @param Recibe un número entero desde la variable "valor2"
+	 * @return Devuelve el resto de ambos
+	 */
+    
+    public int resto (int valor1, int valor2){
+        int numero;
+        numero = valor1 % valor2;
+        return numero;
+    }
+}
