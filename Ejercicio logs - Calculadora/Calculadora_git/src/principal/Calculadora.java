7c451c0cd365f0b5390cc44ef702cf7c87c1b6e6
Pablo5454 <146178829+Pablo5454@users.noreply.github.com>
2024-02-28 20:00:29 +0100

    Ejercicio logs - Calculadora

---
+++ Ejercicio logs - Calculadora/Calculadora_git/src/principal/Calculadora.java
@@ -0,0 +1,92 @@
+package principal;
+
+
+import java.io.IOException;
+import java.lang.annotation.Retention;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+
+import menu.Menu;
+import operaciones.Operaciones;
+
+public class Calculadora {
+
+	/**
+	 * Importa el menu y las operaciones de las clases "Operaciones" y "Menu".
+	 * 
+	 * @see operaciones.Operaciones
+	 * @see menu.Menu
+	 */
+	private static final Logger LOGGER = Logger.getLogger("LOG1");
+
+	public static void main(String[] args) throws SecurityException {
+		
+		LOGGER.setLevel(Level.ALL);
+		LOGGER.setUseParentHandlers(false);
+
+		int resultado = 0;
+		String operacion = "";
+		int[] operandos = new int[2];
+
+		Handler consoleHandler = new ConsoleHandler();
+		Handler fileHandler = null;
+		consoleHandler.setLevel(Level.WARNING);
+		
+		try {
+			fileHandler = new FileHandler("./logs/resultadosCalculadora.txt", true);
+			LOGGER.addHandler(consoleHandler);
+		} catch (SecurityException except) {
+			except.printStackTrace();
+		} catch (IOException except) {
+			except.printStackTrace();
+		}
+
+		LOGGER.addHandler(fileHandler);
+
+		consoleHandler.setLevel(Level.ALL);
+		
+		
+		Menu menu = new Menu();
+		Operaciones operaciones = new Operaciones();
+
+		do {
+			operandos = menu.pedirNumeros();
+			operacion = menu.menuOpciones();
+
+			if (operacion.equalsIgnoreCase("+")) {
+				resultado = operaciones.sumar(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+				LOGGER.log(Level.INFO, "SUMA CORRECTA");
+				
+			} else if (operacion.equalsIgnoreCase("-")) {
+				resultado = operaciones.restar(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+				LOGGER.log(Level.FINE, "Resta correcta.");
+			} else if (operacion.equalsIgnoreCase("*")) {
+				resultado = operaciones.multiplicar(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+				LOGGER.log(Level.FINE, "Multiplicación correcta.");
+			} else if (operacion.equalsIgnoreCase("/")) {
+				try {
+					resultado = operaciones.dividir(operandos[0], operandos[1]);
+					LOGGER.log(Level.FINE, "Division realizada correctamente.");
+				} catch (ArithmeticException e) {
+					LOGGER.log(Level.WARNING, "ERROR! Se ha intentado dividir por cero");
+					resultado = 0;
+				}
+				System.out.println("Resultado: " + resultado);
+			} else if (operacion.equalsIgnoreCase("%")) {
+				resultado = operaciones.resto(operandos[0], operandos[1]);
+				System.out.println("Resultado: " + resultado);
+				LOGGER.log(Level.FINE, "Operación de resto correcta.");
+			} else {
+				System.out.println("Operaci�n no v�lida");
+			}
+		} while (menu.repetir());
+	}
+}
